329272341424085231522c9ad2a9a42de98924cc
FramptMC <faquee@yandex.ru>
2023-08-27 18:36:01 +0400

    lesson 1

---
+++ Non-existent group/Day1/lesson1.py
@@ -0,0 +1,15 @@
+name = "Tornike"
+
+surname = "Nanobashvili"
+
+age = 25
+
+height = 175.3
+
+knows_how_to_code = "Lmao"
+
+Big_balls = True 
+
+
+print(name + " var " + surname + " " + str(age) + " wlis " + str(height) + " cm simaglis " + knows_how_to_code + " ")
+
